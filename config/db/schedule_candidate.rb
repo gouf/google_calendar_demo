d84d69568fbb698aba2f15815ee03a3595b87336
gouf <innocent.zero@gmail.com>
2019-08-25 00:32:46 +0900

    Add database definitions

---
+++ config/db/schedule_candidate.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'active_record'
+require 'yaml'
+
+config = YAML.load_file('./config/db.yml')
+# p config # for debug
+
+ActiveRecord::Base.establish_connection config['development']
+ActiveRecord::Schema.define do
+  unless ActiveRecord::Base.connection.tables.include? 'schedule_candidates'
+    create_table :schedule_candidates do |table|
+      table.column :schedule_id,  :integer
+      table.column :datetime,     :datetime
+    end
+  end
+end
