0a444d7fa2193040ce72089d736e3301e2b4bf1c
gouf <innocent.zero@gmail.com>
2019-09-24 23:29:32 +0900

    main.rb コードの入れ替え

--- main.rb
+++ main.rb
@@ -1,40 +1,24 @@
 # frozen_string_literal: true
-
+require 'date'
 require File.join(__dir__, 'config', 'load_database_and_models')
+require File.join(__dir__, 'lib', 'calendar_event_man', 'calendar_event_man')
 
-#
-# SPEC:
-# 1件の面談スケジュール(未確定) からスケジュール候補日群を作成
-# 作成したスケジュール候補日は Google Calendar に登録する
-# 会社との連絡で面談日が確定したときに、確定処理を実行することで
-# 候補日を Google Calendar 上から削除する (確定日だけが残る)
-#
-# 複数件の面談スケジュールとその候補日を管理できる
-#
+puts '面談日の候補に対する操作'
+puts '1: 複数候補日の作成'
+puts '2: 候補日から確定'
 
-# スケジュール レコードの親を作成
-schedule = Schedule.find_or_create_by(id: 1)
+choise = {
+  '1' => :candidate_creation,
+  '2' => :candidate_decision
+}[gets.chomp]
 
-# ScheduleCandidate.destroy_all
+event_man =
+  case choise
+  when :candidate_creation
+    CalendarEventMan::CandidateCreation.new
+  when :candidate_decision
+    CalendarEventMan::CandidateDecision.new
+  end
 
-schedule_candidates = [
-  DateTime.new(2019, 9, 1, 14, 0, 0, '+9'),
-  DateTime.new(2019, 9, 2, 14, 0, 0, '+9'),
-  DateTime.new(2019, 9, 3, 14, 0, 0, '+9')
-]
-
-# スケジュール候補日を親と関連付けて作成
-Schedule.transaction do
-  schedule.create_candidates(schedule_candidates)
-end
-
-# pp Schedule.all
-# pp ScheduleCandidate.all
-pp Schedule.first.schedule_candidates # 1件の面談の候補日情報をリストアップ
-# pp ScheduleCandidate.first.schedule
-
-# TODO: 候補日3つを入力する UI の作成
-# TODO: 候補日から確定日を選び取る UI の作成
-# TODO: スケジュールが確定した際の候補日の削除処理 (レコード, Google Calendar API)
-# TODO: スケジュール候補日作成時に「summary」「location」などのパラメータを受け取れるようにする
-# TODO: スケジュール候補日削除時に Google Calendar 側の予定も削除できるよう、API 側が管理しているID をレコードとして保存できるようにする
+event_man.ask_all_for_need
+event_man.proceed_task!
