69ea82c991936bae6ed47bf591b4833df0bedc60
gouf <innocent.zero@gmail.com>
2019-10-15 00:54:23 +0900

    refactor: move method to top reference

--- lib/calendar_event_man/candidate_decision.rb
+++ lib/calendar_event_man/candidate_decision.rb
@@ -43,20 +43,9 @@ class CalendarEventMan
       gets.chomp
     end
 
-    # 日本のタイムゾーンに日時表示を変換 (eg. 2019/09/24 (火) 23:50)
+    # Ref: lib/calendar_event_man/calendar_event_man.rb # format_time_in_jpn
     def format_time_in_jpn(datetime)
-      weekday = %w[
-        日
-        月
-        火
-        水
-        木
-        金
-        土
-      ][datetime.to_date.cwday]
-
-      datetime.in_time_zone('Asia/Tokyo')
-              .strftime("%Y/%m/%d (#{weekday}) %H:%M")
+      ::CalendarEventMan.format_time_in_jpn(datetime)
     end
   end
 end
