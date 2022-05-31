32382b362987617e01b1e48ba1d7d3a4422252a6
gouf <innocent.zero@gmail.com>
2022-05-31 18:58:11 +0900

    helper: session 関連メソッドを追記

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+  def current_user
+    return if session[:user_id].blank?
+
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+
+    @current_user = nil
+  end
 end
