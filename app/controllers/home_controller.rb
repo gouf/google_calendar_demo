9f0f23d602275dc814b8b03bf73e333565b3a5a4
gouf <innocent.zero@gmail.com>
2022-06-01 22:25:12 +0900

    仮実装: 認証・認可後の情報で Google Calendar API を操作

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,35 @@
+require 'google/api_client/client_secrets'
+
 class HomeController < ApplicationController
   skip_before_action :strict_logged_in, only: :index
 
   def index
+    if session[:access_token].present?
+      events
+    end
+  end
+
+  def create
+    # TODO: 仮実装として Google Calendar イベントの作成をする
+  end
+
+  private
+
+  # NOTE: 仮実装
+  # OAuth2 認証を経て得たアクセストークンを用いて、カレンダー イベントのリストを取得
+  # FIXME: refactor; lib/**/*.rb にコードを移動・メソッド分割する
+  # NOTE: refresh token 関連の実装が無い
+  # Ref: https://readysteadycode.com/howto-access-the-google-calendar-api-with-ruby
+  def events
+    client = Signet::OAuth2::Client.new(access_token: session[:access_token])
+
+    Google::Apis::ClientOptions.default.application_name = 'Schedule Candidate'
+    # Google::Apis::ClientOptions.default.application_version = '1.0.0'
+    # client_secrets = Google::APIClient::ClientSecrets.load
+
+    calendar = Google::Apis::CalendarV3::CalendarService.new
+    calendar.authorization = client
+
+    @events = calendar.list_events('primary')
   end
 end
