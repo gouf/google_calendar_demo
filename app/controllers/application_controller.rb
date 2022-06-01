14a4ecfe70a32ff08095f5096f44ec32256b5727
gouf <innocent.zero@gmail.com>
2022-06-01 22:09:02 +0900

    ログイン状態のチェック処理を追加
    非ログイン時にログインページ以外の表示を許可しない

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  before_action :strict_logged_in
+
+  def strict_logged_in
+    return if current_user.present?
+
+    redirect_to root_path
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_action :strict_logged_in, only: :index
+
   def index
   end
 end
