f7f04e0d1830a0704244e0c4515b3b73a47ff42c
gouf <innocent.zero@gmail.com>
2022-06-07 20:58:15 +0900

    Google OAuth2 で要求するスコープの追加
    Calendar API へのアクセスが必要なので calendar 指定を追記

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
            Rails.application.credentials.google[:client_id],
-           Rails.application.credentials.google[:client_secret]
+           Rails.application.credentials.google[:client_secret],
+           { scope: 'email,profile,calendar.events' }
 end
