81da18fab8fc224a7f0fd2de13b64273f590ba3a
gouf <innocent.zero@gmail.com>
2022-06-14 22:15:19 +0900

    refactor: before_action 呼び出しに更新

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,9 @@ require 'google/api_client/client_secrets'
 
 class HomeController < ApplicationController
   skip_before_action :strict_logged_in, only: :index
+  before_action :events, only: :index
 
   def index
-    if session[:access_token].present?
-      events
-    end
   end
 
   def create
@@ -21,6 +19,8 @@ class HomeController < ApplicationController
   # NOTE: refresh token 関連の実装が無い
   # Ref: https://readysteadycode.com/howto-access-the-google-calendar-api-with-ruby
   def events
+    return if session[:access_token].blank?
+
     client = Signet::OAuth2::Client.new(access_token: session[:access_token])
 
     Google::Apis::ClientOptions.default.application_name = 'Schedule Candidate'
